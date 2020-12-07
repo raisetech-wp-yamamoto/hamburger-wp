06a437c15a7fea5c75ab9b29728d5a89fd1c00f3
yy <y.yamamoto1783@gmail.com>
2020-12-08 08:49:11 +0900

    singleページスタイル調整

--- single.php
+++ single.php
@@ -4,108 +4,18 @@
                     <h1 class="c-sub-title"><?php the_title(); ?></h1>
                 </div>
                 <div class="l-main__single-page-contents">
-                    <h2>見出しh2</h2>
-                    <p>Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。</p>
-                    <h3>見出しh3</h3>
-                    <h4>見出しh4</h4>
-                    <h5>見出しh5</h5>
-                    <h6>見出しh6</h6>
-                    <blockquote class="c-blockquote" cite="引用元url">
-                        <p>Blockquote 引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ</p>
-                        引用元：<cite><a href="引用元url">○○○○○○○○○○○○</a></cite>
-                    </blockquote>
-                    <img class="u-img-height50rem" src="<?php echo get_template_directory_uri(); ?>/image/cooked-foods-750073.png" alt="width100%">
-                    <div class="c-single-page-card c-single-page-card--left">
-                        <div class="c-single-page-card__img">
-                            <img src="<?php echo get_template_directory_uri(); ?>/image/cooked-foods-750073.png" alt="side-img">
-                        </div>
-                        <div class="c-single-page-card__description">
-                            <p> テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります</p>
-                        </div>
-                    </div>
-                    <div class="c-single-page-card c-single-page-card--right">
-                        <div class="c-single-page-card__img">
-                            <img src="<?php echo get_template_directory_uri(); ?>/image/cooked-foods-750073.png" alt="side-img">
-                        </div>
-                        <div class="c-single-page-card__description">
-                            <p> テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります</p>
-                        </div>
-                    </div>
-                    <div class="u-img-center">
-                        <img class ="u-size-581x358" src="<?php echo get_template_directory_uri(); ?>/image/cooked-foods-750073.png" alt="center">
-                    </div>
-                    <div class="c-grid-img">
-                        <img src="<?php echo get_template_directory_uri(); ?>/image/cooked-foods-750073.png" alt="grid">
-                        <img src="<?php echo get_template_directory_uri(); ?>/image/cooked-foods-750073.png" alt="grid">
-                        <img src="<?php echo get_template_directory_uri(); ?>/image/cooked-foods-750073.png" alt="grid">
-                        <img src="<?php echo get_template_directory_uri(); ?>/image/cooked-foods-750073.png" alt="grid">
-                        <img src="<?php echo get_template_directory_uri(); ?>/image/cooked-foods-750073.png" alt="grid">
-                        <img src="<?php echo get_template_directory_uri(); ?>/image/cooked-foods-750073.png" alt="grid">
-                        <img src="<?php echo get_template_directory_uri(); ?>/image/cooked-foods-750073.png" alt="grid">
-                        <img src="<?php echo get_template_directory_uri(); ?>/image/cooked-foods-750073.png" alt="grid">
-                        <img src="<?php echo get_template_directory_uri(); ?>/image/cooked-foods-750073.png" alt="grid">
-                    </div>
-                    <ul class="c-list">
-                        <li>1.リストリストリスト</li>
-                        <li>2.リストリストリスト</li>
-                    </ul>
-                    <ul class="c-list">
-                        <li>1.リスト2リスト2リスト2</li>
-                        <li>2.リスト2リスト2リスト2</li>
-                    </ul>
-                    <ul class="c-list">
-                        <li>1.リストリストリスト</li>
-                        <li>2.リストリストリスト</li>
-                    </ul>
-                    <ul class="c-list">
-                        <li>リストリストリスト</li>
-                        <li>リストリストリスト</li>
-                    </ul>
-                    <ul class="c-list">
-                        <li>リスト2リスト2リスト2</li>
-                        <li>リスト2リスト2リスト2</li>
-                    </ul>
-                    <ul class="c-list">
-                        <li>リストリストリスト</li>
-                        <li>リストリストリスト</li>
-                    </ul>
-                    <div class="c-code">
-                        <pre>
-                            <code>
-        &lt;html&gt;
-            &lt;head&gt;
-            &lt;/head&gt;
-            &lt;body&gt;
-            &lt;/body&gt;
-        &lt;/html&gt;
-                            </code>
-                        </pre>
-                    </div>
-                    <table class="c-table">
-                        <tbody>
-                            <tr>
-                                <td>テーブル</td>
-                                <td>テーブル</td>
-                            </tr>
-                            <tr>
-                                <td>テーブル</td>
-                                <td>テーブル</td>
-                            </tr>
-                            <tr>
-                                <td>テーブル</td>
-                                <td>テーブル</td>
-                            </tr>
-                            <tr>
-                                <td>テーブル</td>
-                                <td>テーブル</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <button class="c-button">ボタン</button>
-                    <p>boldboldboldboldboldboldbold</p>
+                <?php
+                    if( have_posts() ) :
+                        while( have_posts() ) :
+                            the_post(); 
+                            the_content(); //パーツ化した部分を呼び出し?>
+                        <?php endwhile;
+                    else :
+                        ?><p>表示する記事がありません</p><?php
+                    endif;
+                    wp_reset_query();
+                ?>
                 </div>
-                
-
             </div>
         </div>
         <?php get_sidebar(); ?>
